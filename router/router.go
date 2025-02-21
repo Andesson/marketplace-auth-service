5028f9e35ae25c7c44072fb40f59e8e0c2281611
Andesson <38335324+Andesson@users.noreply.github.com>
2025-02-21 13:40:32 -0300

    Update router.go

--- router/router.go
+++ router/router.go
@@ -8,7 +8,7 @@ import (
 func Initialize() {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"}, // Substitua com sua URL de frontend
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
